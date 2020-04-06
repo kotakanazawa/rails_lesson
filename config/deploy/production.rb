62a464db302d502d98900442077bcd73e891a1d6
kota kanazawa <kota0818@gmail.com>
2020-04-06 23:10:18 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "kota-kanazawa.work", user: "deploy", roles: %w{web db app}
+server "133.167.121.197", user: "deploy", roles: %w{web db app}
 
 # ssh
 set :ssh_options, {
@@ -8,10 +8,3 @@ set :ssh_options, {
   forward_agent: true,
   auth_methods: %w[publickey]
 }
-
-# nginx
-set :nginx_config_name, "#{fetch(:application)}"
-set :nginx_server_name, "localhost #{fetch(:nginx_config_name)}"
-set :nginx_use_ssl, true
-set :nginx_ssl_certificate, "/etc/letsencrypt/live/#{fetch(:nginx_config_name)}/fullchain.pem"
-set :nginx_ssl_certificate_key, "/etc/letsencrypt/live/#{fetch(:nginx_config_name)}/privkey.pem"
