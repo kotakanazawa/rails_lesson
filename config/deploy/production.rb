123b4a7de721ea745cc604896f314fd016df09c3
kota kanazawa <kota0818@gmail.com>
2020-04-14 11:46:36 +0900

    CD config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server "kota-kanazawa.work", user: "deploy", roles: %w{web db app}
 # ssh
 set :ssh_options, {
   user: "deploy", # TODO change to fetch(:user)
-  port: 10022, # TODO move to the line in the server
+  port: 10022,
   keys: ["#{ENV.fetch('PRODUCTION_SSH_KEY')}"],
   forward_agent: true,
   auth_methods: %w[publickey]
@@ -15,4 +15,3 @@ set :nginx_server_name, "kota-kanazawa.work"
 set :nginx_use_ssl, true
 set :nginx_ssl_certificate, "/etc/letsencrypt/live/kota-kanazawa.work/fullchain.pem"
 set :nginx_ssl_certificate_key, "/etc/letsencrypt/live/kota-kanazawa.work/privkey.pem"
-
